31bfd014e898def08091c22f6477fb25b23506df
user <user@user-PC>
2021-01-30 16:53:07 +0800

    Say oorchilson

--- Circle/src/lab1/linkedlist.java
+++ Circle/src/lab1/linkedlist.java
@@ -6,21 +6,32 @@ public class linkedlist {
 		
 			//oyutnii medeelel awah
 		  OyutniiMedeelel.add("B190910032");
-		  OyutniiMedeelel.add("З.Тэмүүлэн");
-		  OyutniiMedeelel.add("Програм хангамж 2");
-		  OyutniiMedeelel.add("Утас:88956646");
+		  OyutniiMedeelel.add("Ð—.Ð¢Ñ�Ð¼Ò¯Ò¯Ð»Ñ�Ð½");
+		  OyutniiMedeelel.add("ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼ Ñ…Ð°Ð½Ð³Ð°Ð¼Ð¶ 2");
+		  OyutniiMedeelel.add("Ð£Ñ‚Ð°Ñ�:88956646");
 		  OyutniiMedeelel.add("Gmail:ttemvvlen.0717@gmail.com");
 		  
 		   System.out.println(OyutniiMedeelel);
+		   
+		   
+		   
+		   
+		   OyutniiMedeelel.add("B190910803");
+			  OyutniiMedeelel.add("L. Enkhsuren");
+			  OyutniiMedeelel.add("SoftWare");
+			  OyutniiMedeelel.add("Phone: ********");
+			  OyutniiMedeelel.add("Gmail:eenkhsuren10@gmail.com");
+			  
+			   System.out.println(OyutniiMedeelel);
 		    
 		
 		
 		
 		  LinkedList<String> First = new LinkedList<String>();
 		  First.add("B190910032");
-		  First.add("З.Тэмүүлэн");
-		  First.add("Програм хангамж 2");
-		  First.add("Утас:88956646");
+		  First.add("Ð—.Ð¢Ñ�Ð¼Ò¯Ò¯Ð»Ñ�Ð½");
+		  First.add("ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼ Ñ…Ð°Ð½Ð³Ð°Ð¼Ð¶ 2");
+		  First.add("Ð£Ñ‚Ð°Ñ�:88956646");
 		  First.add("Gmail:ttemvvlen.0717@gmail.com");
 		  
 		  //GetFirst ashiglasan
@@ -31,9 +42,9 @@ public class linkedlist {
 		    LinkedList<String> Last = new LinkedList<String>();
 		    
 			  Last.add("B190910032");
-			  Last.add("З.Тэмүүлэн");
-			  Last.add("Програм хангамж 2");
-			  Last.add("Утас:88956646");
+			  Last.add("Ð—.Ð¢Ñ�Ð¼Ò¯Ò¯Ð»Ñ�Ð½");
+			  Last.add("ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼ Ñ…Ð°Ð½Ð³Ð°Ð¼Ð¶ 2");
+			  Last.add("Ð£Ñ‚Ð°Ñ�:88956646");
 			  Last.add("Gmail:ttemvvlen.0717@gmail.com");
 			  
 			  //GetLast ashiglasan
